8ee7ad6d61a589b7c7c75142503d43ffbd8dc988
cchuuu <158204289+cchuuu@users.noreply.github.com>
2024-02-06 12:27:43 -0500

    Create challenge#1.py

---
+++ challenge#1.py
@@ -0,0 +1,19 @@
+x = "Bill"
+y = "Tip"
+z = "TotalAmount"
+
+service = input("How was our service?")
+if service == ("Bad"):
+    print("10% Tip")
+elif service == ("Okay"):
+    print("15% Tip")
+elif service == ("Good"):
+    print("20% Tip")
+elif service == ("Amazing"):
+    print("25% Tip")
+
+Bill = input("How much was your meal?")
+Bill * service * 
+
+
+
