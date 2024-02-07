7d6e1ae6c832836cc0f577541f233c5d12cceddb
cchuuu <158204289+cchuuu@users.noreply.github.com>
2024-02-07 12:26:04 -0500

    Update challenge#1.py

--- challenge#1.py
+++ challenge#1.py
@@ -1,19 +1,30 @@
 x = "Bill"
 y = "Tip"
 z = "TotalAmount"
+total = 0
+tip = 0
+message = "Here's your total."
+
+
+x = float(input("How much was your meal?"))
 
 service = input("How was our service?")
 if service == ("Bad"):
-    print("10% Tip")
+    print("0% Tip")
+    tip = x * 0
 elif service == ("Okay"):
     print("15% Tip")
+    tip = x * 0.15
 elif service == ("Good"):
     print("20% Tip")
-elif service == ("Amazing"):
+    tip = x * 0.2
+elif service == ("Great"):
     print("25% Tip")
+    tip = x *0.25
 
-Bill = input("How much was your meal?")
-Bill * service * 
-
+print (tip)
+total = x + tip
+print (message)
+print (total)
 
 
