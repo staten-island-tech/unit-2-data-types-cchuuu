2a2612cf2995792c2678103703908d049b3d7f50
cchuuu <158204289+cchuuu@users.noreply.github.com>
2024-02-11 16:39:38 -0500

    Update challenge#3.py

--- challenge#3.py
+++ challenge#3.py
@@ -1,5 +1,5 @@
-oddoreven = float(input("What's your number?"))
-if oddoreven%2 == 0:
-    print ("Even")
+oddoreven = float(input("What's your number?")) #asks for input and converts number to decimal
+if oddoreven%2 == 0: #if the remainder of input/2 = 0
+    print ("Even") #print Even
 else:
-    print ("Odd")
+    print ("Odd")  #if the remainder of input/2 is not 0, print Odd
