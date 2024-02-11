5f42a4265f7bb4e84b501e8cc3a31f1a4d92d86c
cchuuu <158204289+cchuuu@users.noreply.github.com>
2024-02-11 16:39:40 -0500

    Update challenge#4.py

--- challenge#4.py
+++ challenge#4.py
@@ -1,13 +1,13 @@
-number = float(input("What is your number?"))
-num = int(number)
-factors = []
+number = float(input("What is your number?")) #asks for input and converts it to a decimal
+num = int(number) #converts the input to a integer
+factors = [] #the factors will go into the brackets
 
-for i in range(1,int(num/2)):
-    if num%i == 0:
-        if i not in factors:
-            factors.append(i)
-        if number/i not in factors:
-            factors.append(int(num/i))
+for i in range(1,int(num/2)): #i = factors, in range of 1 and the input divided by 2
+    if num%i == 0: #if the remainder of input/i (one of the numbers in the range) = 0, moves onto next code
+        if i not in factors: #if i is not in factors
+            factors.append(i) #add i into factors
+        if number/i not in factors: #if input/i is not in factors
+            factors.append(int(num/i)) #add i (converted to integer) into factors
 
-for i in factors:
-    print(i)
+for i in factors: #for values in factors
+    print(i) #print the factors
