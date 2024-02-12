7ff9e9741182b27c3ee29ac596e79d67f0ed6a63
cchuuu <158204289+cchuuu@users.noreply.github.com>
2024-02-12 12:12:36 -0500

    Create challenge#5(2).py

---
+++ challenge#5(2).py
@@ -0,0 +1,14 @@
+number = int(input("What is your number?")) #asks for input and converts it to integer 
+number2 = int(input("What is your second number?")) #asks for second input and converts it to integer
+factors = []
+
+for i in range (1, number+1): #i in range of 1 and (first input + 1)
+    if (number%i == 0) and (number2%i == 0): #if the remainder of first input and second input is 0
+        factors.append(i)
+
+print(f"This is the GCF, {factors[-1]}")
+            
+
+
+
+
