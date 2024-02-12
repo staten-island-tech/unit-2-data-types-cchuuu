65fe5603dd5eb0636ef2fa74139f15b7acc314b4
cchuuu <158204289+cchuuu@users.noreply.github.com>
2024-02-11 20:09:54 -0500

    Update challenge#2.py

--- challenge#2.py
+++ challenge#2.py
@@ -1,4 +1,4 @@
 x = input() #asks for input 
-numberletters = x.split ( ) 
-print(len(numberletters))
+numberletters = x.split ( ) #the words in the input are split up by ( ) (spaces) and numberletters = 
+print(len(numberletters)) #prints the # of words
 
