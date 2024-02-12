9059ba2a90f289b5afbda2c4022beea1d1a87495
cchuuu <158204289+cchuuu@users.noreply.github.com>
2024-02-11 20:10:00 -0500

    Update challenge#5.py

--- challenge#5.py
+++ challenge#5.py
@@ -6,8 +6,8 @@ num = int(number) #converts first input to integer
 num2 = int(number2) #converts second input to integer
 
 x = math.gcd(num,num2) #finds the greatest common denominators/factors of first input and second input
-print ("This is the greatest common factor") #prints message
-print (x) #prints the greatest common denominators/factors
+print (f"This is the greatest common factor: {x}") #prints message & the greatest common factor
+
 
  
 
