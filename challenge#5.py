4f4a95e4dba97c4c53dcfd8be035fe44ce21f2e9
cchuuu <158204289+cchuuu@users.noreply.github.com>
2024-02-11 22:54:17 -0500

    Update challenge#5.py

--- challenge#5.py
+++ challenge#5.py
@@ -7,13 +7,13 @@ def findgcf(number,number2): #function
         small = number2
     else: 
         small = number 
-    for i in range (1, number+1):
-        if (number%i == 0) and (number2%i == 0):
-            gcf = i
+    for i in range (1, number+1): #i in range of 1 and (first input + 1)
+        if (number%i == 0) and (number2%i == 0): #if the remainder of first input and second input is 0
+            gcf = i #
 
-    return(gcf)
+    return(gcf) #returns the value to the function
 
-print(f"This is the GCF {findgcf(number,number2)}")
+print(f"This is the GCF, {findgcf(number,number2)}")
             
 
 
