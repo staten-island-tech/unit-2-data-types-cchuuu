d661450645e64f4c03abaf8a1dce397647270ba9
cchuuu <158204289+cchuuu@users.noreply.github.com>
2024-02-11 22:46:27 -0500

    Update challenge#5.py

--- challenge#5.py
+++ challenge#5.py
@@ -1,14 +1,21 @@
-import math #imports the math library
-
-number = float(input("What is your number?")) #asks for input and converts it to decimal
-number2 = float(input("What is your second number?")) #asks for second input and converts it to decimal
-num = int(number) #converts first input to integer
-num2 = int(number2) #converts second input to integer
-
-x = math.gcd(num,num2) #finds the greatest common denominators/factors of first input and second input
-print (f"This is the greatest common factor: {x}") #prints message & the greatest common factor
+number = int(input("What is your number?")) #asks for input and converts it to integer 
+number2 = int(input("What is your second number?")) #asks for second input and converts it to integer
 
 
- 
+def findgcf(number,number2): #function 
+    if number > number2: 
+        small = number2
+    else: 
+        small = number 
+    for i in range (1, number+1):
+        if (number%i == 0) and (number2%i == 0):
+            gcf = i
+
+    return(gcf)
+
+print(f"This is the GCF {findgcf(number,number2)}")
+            
+
+
 
 
